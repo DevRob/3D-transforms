6551f4b58af463ca476da7091389275c07e5c6be
Robert Molnar <mail.robertmolnar@gmail.com>
2016-11-24 19:20:42 +0000

    css and utils.js upd

---
+++ js/rotate-box.js
@@ -0,0 +1,68 @@
+	var layers = [],
+		objects = [],
+
+		world = document.getElementById( 'world' ),
+		viewport = document.getElementById( 'viewport' ),
+
+		d = 0,
+		p = 400,
+		worldXAngle = 0,
+		worldYAngle = 0;
+
+	viewport.style.webkitPerspective = p;
+	viewport.style.MozPerspective = p;
+	viewport.style.oPerspective = p;
+
+	generate();
+
+	function createCloud() {
+
+		var div = document.createElement( 'div'  );
+		div.className = 'cloudBase';
+		var x = 120;
+		var y = 86;
+		var z = 135;
+		var t = 'translateX( ' + x + 'px ) translateY( ' + y + 'px ) translateZ( ' + z + 'px )';
+		div.style.webkitTransform = t;
+		div.style.MozTransform = t;
+		div.style.oTransform = t;
+		world.appendChild( div );
+
+		return div;
+	}
+
+	window.addEventListener( 'mousewheel', onContainerMouseWheel );
+	window.addEventListener( 'DOMMouseScroll', onContainerMouseWheel );
+
+	window.addEventListener( 'mousemove', function( e ) {
+		worldYAngle = -( .5 - ( e.clientX / window.innerWidth ) ) * 180;
+		worldXAngle = ( .5 - ( e.clientY / window.innerHeight ) ) * 180;
+		updateView();
+	} );
+
+	function generate() {
+		objects = [];
+		if ( world.hasChildNodes() ) {
+			while ( world.childNodes.length >= 1 ) {
+				world.removeChild( world.firstChild );
+			}
+		}
+		for( var j = 0; j < 5; j++ ) {
+			objects.push( createCloud() );
+		}
+	}
+
+	function updateView() {
+		var t = 'translateZ( ' + d + 'px ) rotateX( ' + worldXAngle + 'deg) rotateY( ' + worldYAngle + 'deg)';
+		world.style.webkitTransform = t;
+		world.style.MozTransform = t;
+		world.style.oTransform = t;
+	}
+
+	function onContainerMouseWheel( event ) {
+
+		event = event ? event : window.event;
+		d = d - ( event.detail ? event.detail * -5 : event.wheelDelta / 8 );
+		updateView();
+
+	}
